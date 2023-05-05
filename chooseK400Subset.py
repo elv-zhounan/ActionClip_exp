e7bdd3d334c8e9e4b73523139c76b9d6386cdb71
elv-zhounan <zhounan@eluv.io>
2023-05-05 01:36:26 +0000

    fix bugs

--- chooseK400Subset.py
+++ chooseK400Subset.py
@@ -3,8 +3,8 @@ import json
 import tqdm
 
 train_root = "/pool0/ml/elv-zhounan/action/kinetics/k400/images/train"
-subset_num = 50
-
+subset_num = 100
+offset = 100
 classes = os.listdir(train_root)
 
 cnt = {}
@@ -18,6 +18,6 @@ _sorted = sorted(cnt.items(), key=lambda x:x[1], reverse=True)
 
 print(_sorted)
 
-subset = {k:v for (k,  v) in _sorted[:subset_num]}
+subset = {k:v for (k,  v) in _sorted[offset:offset+subset_num]}
 
 json.dump(subset, open(f"/home/elv-zhounan/ActionClip_exp/cfg/exp/k400/subset{subset_num}.json", "w"))
